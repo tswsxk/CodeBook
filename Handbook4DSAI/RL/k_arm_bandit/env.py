0999736a1b1a3c8b6e0e0eac94b8876ca9adddd2
tswsxk <tongsw@mail.ustc.edu.cn>
2020-01-18 14:01:38 +0800

    update

---
+++ Handbook4DSAI/RL/k_arm_bandit/env.py
@@ -0,0 +1,30 @@
+# coding: utf-8
+# 2019/12/24 @ tongshiwei
+
+import gym
+
+from gym import Space
+
+class KABActionSpace(Space):
+
+
+
+
+class KArmBandit(gym.Env):
+    def __init__(self):
+        pass
+
+    def reset(self):
+        pass
+
+    def step(self, action):
+        pass
+
+    def render(self, mode='human'):
+        pass
+
+    def close(self):
+        pass
+
+    def action_space(self):
+        pass
